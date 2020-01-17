99e8bf02f491b6f27e597ffae7209122dcc722c3
Pedro Vicente <pedrovicente@poli.ufrj.br>
2020-01-17 12:51:04 -0300

    Alteracao page_not_found

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ import json
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    response = "Page not found."
+    response = jsonify(response)
+    response.status_code = 404
+    return response
 
 @app.route('/artistas', methods=['GET'])
 def get_artists():
