6f239009945a6a29ee73f004497a389d3898cd16
Pedro Vicente <pedrovicente@poli.ufrj.br>
2020-01-19 14:43:31 -0300

    Adicionada delete_all_songs para deletar todos as musicas de uma vez so

--- app/main.py
+++ app/main.py
@@ -838,5 +838,24 @@ def delete_all_albuns():
       cursor.close()
       connection.close()
 
+@app.route('/musicas', methods=['DELETE'])
+def delete_all_songs():
+  try:
+    sql = "DELETE FROM songs"
+    connection = connect_to_db()
+    if (connection.is_connected()):
+      cursor = connection.cursor()
+      cursor.execute(sql)
+      connection.commit()
+      response = jsonify("Deletion successful")
+      response.status_code = 204
+      return response
+  except Exception as e:
+    return "Error: " + str(e)
+  finally:
+    if (connection.is_connected()):
+      cursor.close()
+      connection.close()
+
 if __name__ == '__main__':
   app.run(debug=True,host = '0.0.0.0')
