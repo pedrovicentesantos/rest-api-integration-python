9410e779bc12dace6832a4c5bf4999fc610237d6
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2022-02-11 21:11:21 -0300

    refactor: db tables structure

--- db/init.sql
+++ db/init.sql
@@ -2,45 +2,32 @@ CREATE DATABASE IF NOT EXISTS artists_db;
 USE artists_db;
 
 CREATE TABLE IF NOT EXISTS artists (
-  `idArtist` INT NOT NULL AUTO_INCREMENT,
-  `nameArtist` VARCHAR(255) NOT NULL,
-  `idArtistItunes` BIGINT NULL,
-  PRIMARY KEY (`idArtist`));
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `genre` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS albuns (
-  `idAlbum` INT NOT NULL AUTO_INCREMENT,
-  `nameAlbum` VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS albums (
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   `trackCount` INT NOT NULL,
   `explicit` VARCHAR(45) NOT NULL,
   `genre` VARCHAR(45) NOT NULL,
-  `idAlbumItunes` BIGINT NOT NULL,
-  `nameArtistAlbum` VARCHAR(255) NOT NULL,
-  `idArtistAlbum` INT NOT NULL,
-  PRIMARY KEY (`idAlbum`),
-  INDEX `idArstistAlbum_idx` (`idArtistAlbum` ASC) VISIBLE,
-  CONSTRAINT `idArtistAlbum`
-    FOREIGN KEY (`idArtistAlbum`)
-    REFERENCES artists (`idArtist`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
+  `artistName` VARCHAR(255) NOT NULL,
+  `artistId` INT NOT NULL,
+  `releaseDate` DATE NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
 	
-CREATE TABLE songs (
-  `idSong` INT NOT NULL AUTO_INCREMENT,
-  `nameSong` VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS songs (
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   `explicit` VARCHAR(45) NOT NULL,
   `genre` VARCHAR(45) NOT NULL,
-  `idSongItunes` BIGINT NOT NULL,
-  `nameArtistSong` VARCHAR(255) NOT NULL,
-  `nameAlbumSong` VARCHAR(255) NOT NULL,
-  `idAlbumSong` INT NOT NULL,
-  PRIMARY KEY (`idSong`),
-  INDEX `idAlbumSong_idx` (`idAlbumSong` ASC) VISIBLE,
-  CONSTRAINT `idAlbumSong`
-    FOREIGN KEY (`idAlbumSong`)
-    REFERENCES albuns (`idAlbum`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
-
-
-
-  
+  `artistName` VARCHAR(255) NOT NULL,
+  `artistId` INT NOT NULL,
+  `albumName` VARCHAR(255) NOT NULL,
+  `albumId` INT NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `durationMinutes` REAL NOT NULL,
+  PRIMARY KEY (`id`));
